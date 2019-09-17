61bd2cb8e85e08e2eff45f46c68257cad712d5f2
Асус <https://github.com/Ushiramaru/OOP_1.git>
2019-09-18 02:20:20 +0300

    Other check of leap

--- src/SecondBlock/Task_4/Data.java
+++ src/SecondBlock/Task_4/Data.java
@@ -43,14 +43,7 @@ public class Data {
     }
 
     private boolean isLeap(int year) {
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                return year % 400 == 0;
-            } else {
-                return true;
-            }
-        }
-        return false;
+        return (year % 4 == 0) & (year % 100 != 0) | (year % 400 == 0);
     }
 
     public void nextDay() {
