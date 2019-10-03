589e46af56b8dfe4329ea28af4ff9a60a64b7db6
Асус <https://github.com/Ushiramaru/OOP_1.git>
2019-10-03 03:51:24 +0300

    Added SRP for methods, optimized algorithm by find local values, delete Main(because after review)

--- src/mainTask_3/Matrix.java
+++ src/mainTask_3/Matrix.java
@@ -1,6 +1,5 @@
 package mainTask_3;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Matrix {
@@ -14,124 +13,313 @@ public class Matrix {
         }
         double[][] matrix = new double[rowCount][colCount];
         Random random = new Random();
-        for (int row = 0; row < rowCount; row++) {
-            for (int col = 0; col < colCount; col++) {
-                matrix[row][col] = random.nextInt(maxValue - minValue) + minValue + random.nextDouble();
+        if (minValue != maxValue) {
+            for (int row = 0; row < rowCount; row++) {
+                for (int col = 0; col < colCount; col++) {
+                    matrix[row][col] = random.nextInt(maxValue - minValue) + minValue + random.nextDouble();
+                }
+            }
+        } else {
+            for (int row = 0; row < rowCount; row++) {
+                for (int col = 0; col < colCount; col++) {
+                    matrix[row][col] = minValue;
+                }
             }
         }
         return matrix;
     }
 
-    public static double[] extremeValues(double[][] matrix) {
-        for (int row = 0; row < matrix.length - 1; row++) {
-            if (matrix[row].length != matrix[row + 1].length) {
-                throw new IllegalArgumentException("Array must be matrix");
+    public static double minValue(double[][] matrix) throws Exception {
+        if (matrix == null) {
+            throw new NullPointerException("Matrix can`t be null");
+        }
+        for (double[] row : matrix) {
+            if (row == null) {
+                throw new NullPointerException("Row of matrix can`t be null");
             }
         }
-        double[] extreme = new double[2];
-        extreme[0] = matrix[0][0];
-        extreme[1] = matrix[0][0];
+        if (!isMatrix(matrix)) {
+            throw new IllegalArgumentException("Array must be matrix");
+        }
+        if (isOneValue(matrix)) {
+            throw new Exception("Matrix hasn't min value");
+        }
+        double minValue = matrix[0][0];
         for (double[] row : matrix) {
             for (double cell : row) {
-                if (extreme[0] > cell) {
-                    extreme[0] = cell;
-                } else {
-                    if (extreme[1] < cell) {
-                        extreme[1] = cell;
-                    }
+                if (cell < minValue) {
+                    minValue = cell;
                 }
             }
         }
-        return extreme;
+        return minValue;
     }
 
-    public static double[] averageValues(double[][] matrix) {
-        for (int row = 0; row < matrix.length - 1; row++) {
-            if (matrix[row].length != matrix[row + 1].length) {
-                throw new IllegalArgumentException("Array must be matrix");
+    public static double maxValue(double[][] matrix) throws Exception {
+        if (matrix == null) {
+            throw new NullPointerException("Matrix can`t be null");
+        }
+        for (double[] row : matrix) {
+            if (row == null) {
+                throw new NullPointerException("Row of matrix can`t be null");
             }
         }
-        double[] average = new double[2];
-        average[0] = 0;
-        average[1] = 1;
+        if (!isMatrix(matrix)) {
+            throw new IllegalArgumentException("Array must be matrix");
+        }
+        if (isOneValue(matrix)) {
+            throw new Exception("Matrix hasn't max value");
+        }
+
+        double maxValue = matrix[0][0];
         for (double[] row : matrix) {
             for (double cell : row) {
-                average[0] += cell;
-                average[1] *= cell;
+                if (cell > maxValue) {
+                    maxValue = cell;
+                }
             }
         }
-        average[0] = average[0] / (matrix.length * matrix[0].length);
-        average[1] = Math.pow(average[1], 1d / (matrix.length * matrix[0].length));
-        return average;
+        return maxValue;
     }
 
-    public static int[][] localExtremesPosition(double[][] matrix) {
-        for (int row = 0; row < matrix.length - 1; row++) {
-            if (matrix[row].length != matrix[row + 1].length) {
-                throw new IllegalArgumentException("Array must be matrix");
+    public static double arithmeticAverageValue(double[][] matrix) {
+        if (matrix == null) {
+            throw new NullPointerException("Matrix can`t be null");
+        }
+        for (double[] row : matrix) {
+            if (row == null) {
+                throw new NullPointerException("Row of matrix can`t be null");
             }
         }
-        int[][] position = new int[2][];
-        position[0] = new int[2];
-        position[0][0] = -1;
-        position[0][1] = -1;
-        position[1] = new int[2];
-        position[1][0] = -1;
-        position[1][1] = -1;
-        for (int row = 0; row < matrix.length && (Arrays.equals(position[0], new int[]{-1, -1}) || Arrays.equals(position[1], new int[]{-1, -1})); row++) {
-            for (int col = 0; col < matrix[row].length && (Arrays.equals(position[0], new int[]{-1, -1}) || Arrays.equals(position[1], new int[]{-1, -1})); col++) {
-                boolean isLocalMin = true;
-                boolean isLocalMax = true;
-                if (row > 0 && col > 0) {
-                    if (matrix[row][col] >= matrix[row - 1][col]) {
-                        isLocalMin = false;
-                        if (matrix[row][col] == matrix[row - 1][col]) {
-                            isLocalMax = false;
-                        }
-                    }
-                }
-                if (col > 0) {
-                    if (matrix[row][col] >= matrix[row][col - 1]) {
-                        isLocalMin = false;
-                        if (matrix[row][col] == matrix[row][col - 1]) {
-                            isLocalMax = false;
-                        }
-                    }
-                }
-                if (row + 1 < matrix.length && col + 1 < matrix[row].length) {
-                    if (matrix[row][col] >= matrix[row + 1][col]) {
-                        isLocalMin = false;
-                        if (matrix[row][col] == matrix[row + 1][col]) {
-                            isLocalMax = false;
-                        }
-                    }
-                }
-                if (col + 1 < matrix[row].length) {
-                    if (matrix[row][col] >= matrix[row][col + 1]) {
-                        isLocalMin = false;
-                        if (matrix[row][col] == matrix[row][col + 1]) {
-                            isLocalMax = false;
-                        }
-                    }
-                }
-                if (isLocalMin && Arrays.equals(position[0], new int[]{-1, -1})) {
-                    position[0] = new int[]{row, col};
-                } else {
-                    if (isLocalMax && Arrays.equals(position[1], new int[]{-1, -1})) {
-                        position[1] = new int[]{row, col};
-                    }
-                }
+        if (!isMatrix(matrix)) {
+            throw new IllegalArgumentException("Array must be matrix");
+        }
+
+        int average = 0;
+        for (double[] row : matrix) {
+            for (double cell : row) {
+                average += cell;
             }
         }
-        return position;
+        return average / matrix.length * matrix[0].length;
     }
 
+    public static double geometricAverageValue(double[][] matrix) {
+        if (matrix == null) {
+            throw new NullPointerException("Matrix can`t be null");
+        }
+        for (double[] row : matrix) {
+            if (row == null) {
+                throw new NullPointerException("Row of matrix can`t be null");
+            }
+        }
+        if (!isMatrix(matrix)) {
+            throw new IllegalArgumentException("Array must be matrix");
+        }
+
+        int average = 1;
+        for (double[] row : matrix) {
+            for (double cell : row) {
+                average *= cell;
+            }
+        }
+        return Math.pow(average, 1d / (matrix.length * matrix[0].length));
+    }
+
+    public static int[] firstLocalMinPosition(double[][] matrix) {
+        if (matrix == null) {
+            throw new NullPointerException("Matrix can`t be null");
+        }
+        for (double[] row : matrix) {
+            if (row == null) {
+                throw new NullPointerException("Row of matrix can`t be null");
+            }
+        }
+        if (!isMatrix(matrix)) {
+            throw new IllegalArgumentException("Array must be matrix");
+        }
+
+        if (isOneValue(matrix)) {
+            return new int[]{-1, -1};
+        }
+
+        if (matrix.length == 1) {
+            for (int i = 0; i < matrix[0].length -  1; i++) {
+                if (matrix[0][i] < matrix[0][i + 1]) {
+                    return new int[]{0, i};
+                }
+            }
+            return new int[]{0, matrix[0].length - 1};
+        }
+        if (matrix[0].length == 1) {
+            for (int i = 0; i < matrix.length -  1; i++) {
+                if (matrix[0][i] < matrix[i + 1][0]) {
+                    return new int[]{i, 0};
+                }
+            }
+            return new int[]{matrix.length - 1, 0};
+        }
+
+        if ((matrix[0][0] < matrix[1][0]) &&
+                (matrix[0][0] < matrix[0][1])) {
+            return new int[]{0, 0};
+        }
+        for (int i = 1; i < matrix[0].length - 1; i++) {
+            if ((matrix[0][i] < matrix[0][i - 1]) &&
+                    (matrix[0][i] < matrix[1][i]) &&
+                    (matrix[0][i] < matrix[0][i + 1])) {
+                return new int[]{0, i};
+            }
+        }
+        if ((matrix[0][matrix[0].length - 1] < matrix[1][matrix[0].length - 1]) &&
+                (matrix[0][matrix[0].length - 1] < matrix[0][matrix[0].length - 2])) {
+            return new int[]{0, matrix[0].length - 1};
+        }
+
+        for (int i = 1; i < matrix.length - 1; i++) {
+            if ((matrix[i][0] < matrix[i - 1][0]) &&
+                    (matrix[i][0] < matrix[i][1]) &&
+                    (matrix[i][0] < matrix[i + 1][0])) {
+                return new int[]{i, 0};
+            }
+            for (int j = 1; j < matrix[0].length - 1; j++) {
+                if ((matrix[i][j] < matrix[i][j - 1]) &&
+                        (matrix[i][j] < matrix[i + 1][j]) &&
+                        (matrix[i][j] < matrix[i][j + 1]) &&
+                        (matrix[i][j] < matrix[i + 1][j])) {
+                    return new int[]{i, j};
+                }
+            }
+            if ((matrix[i][matrix[0].length - 1] < matrix[i + 1][matrix[0].length - 1]) &&
+                    (matrix[i][matrix[0].length - 1] < matrix[i][matrix[0].length - 2]) &&
+                    (matrix[i][matrix[0].length - 1] < matrix[i - 1][matrix[0].length - 1])) {
+                return new int[]{i, matrix[0].length - 1};
+            }
+        }
+
+        if ((matrix[matrix.length - 1][0] < matrix[matrix.length - 2][0]) &&
+                (matrix[matrix.length - 1][0] < matrix[matrix.length - 1][1])) {
+            return new int[]{matrix.length - 1, 0};
+        }
+        for (int i = 1; i < matrix[0].length - 1; i++) {
+            if ((matrix[matrix.length - 1][i] < matrix[0][i - 1]) &&
+                    (matrix[matrix.length - 1][i] < matrix[matrix.length - 2][i]) &&
+                    (matrix[matrix.length - 1][i] < matrix[matrix.length - 1][i + 1])) {
+                return new int[]{matrix.length - 1, i};
+            }
+        }
+        if ((matrix[matrix.length - 1][matrix[0].length - 1] < matrix[matrix.length - 1][matrix[0].length - 2]) &&
+                (matrix[matrix.length - 1][matrix[0].length - 1] < matrix[matrix.length - 2][matrix[0].length - 1])) {
+            return new int[]{matrix.length - 1, matrix[0].length - 1};
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static int[] firstLocalMaxPosition(double[][] matrix) {
+        if (matrix == null) {
+            throw new NullPointerException("Matrix can`t be null");
+        }
+        for (double[] row : matrix) {
+            if (row == null) {
+                throw new NullPointerException("Row of matrix can`t be null");
+            }
+        }
+        if (!isMatrix(matrix)) {
+            throw new IllegalArgumentException("Array must be matrix");
+        }
+
+        if (isOneValue(matrix)) {
+            return new int[]{-1, -1};
+        }
+
+        if (matrix.length == 1) {
+            for (int i = 0; i < matrix[0].length -  1; i++) {
+                if (matrix[0][i] > matrix[0][i + 1]) {
+                    return new int[]{0, i};
+                }
+            }
+            return new int[]{0, matrix[0].length - 1};
+        }
+        if (matrix[0].length == 1) {
+            for (int i = 0; i < matrix.length -  1; i++) {
+                if (matrix[0][i] > matrix[i + 1][0]) {
+                    return new int[]{i, 0};
+                }
+            }
+            return new int[]{matrix.length - 1, 0};
+        }
+
+        if ((matrix[0][0] < matrix[1][0]) &&
+                (matrix[0][0] < matrix[0][1])) {
+            return new int[]{0, 0};
+        }
+        for (int i = 1; i < matrix[0].length - 1; i++) {
+            if ((matrix[0][i] > matrix[0][i - 1]) &&
+                    (matrix[0][i] > matrix[1][i]) &&
+                    (matrix[0][i] > matrix[0][i + 1])) {
+                return new int[]{0, i};
+            }
+        }
+        if ((matrix[0][matrix[0].length - 1] > matrix[1][matrix[0].length - 1]) &&
+                (matrix[0][matrix[0].length - 1] > matrix[0][matrix[0].length - 2])) {
+            return new int[]{0, matrix[0].length - 1};
+        }
+
+        for (int i = 1; i < matrix.length - 1; i++) {
+            if ((matrix[i][0] > matrix[i - 1][0]) &&
+                    (matrix[i][0] > matrix[i][1]) &&
+                    (matrix[i][0] > matrix[i + 1][0])) {
+                return new int[]{i, 0};
+            }
+            for (int j = 1; j < matrix[0].length - 1; j++) {
+                if ((matrix[i][j] > matrix[i][j - 1]) &&
+                        (matrix[i][j] > matrix[i + 1][j]) &&
+                        (matrix[i][j] > matrix[i][j + 1]) &&
+                        (matrix[i][j] > matrix[i + 1][j])) {
+                    return new int[]{i, j};
+                }
+            }
+            if ((matrix[i][matrix[0].length - 1] > matrix[i + 1][matrix[0].length - 1]) &&
+                    (matrix[i][matrix[0].length - 1] > matrix[i][matrix[0].length - 2]) &&
+                    (matrix[i][matrix[0].length - 1] > matrix[i - 1][matrix[0].length - 1])) {
+                return new int[]{i, matrix[0].length - 1};
+            }
+        }
+
+        if ((matrix[matrix.length - 1][0] > matrix[matrix.length - 2][0]) &&
+                (matrix[matrix.length - 1][0] > matrix[matrix.length - 1][1])) {
+            return new int[]{matrix.length - 1, 0};
+        }
+        for (int i = 1; i < matrix[0].length - 1; i++) {
+            if ((matrix[matrix.length - 1][i] > matrix[0][i - 1]) &&
+                    (matrix[matrix.length - 1][i] > matrix[matrix.length - 2][i]) &&
+                    (matrix[matrix.length - 1][i] > matrix[matrix.length - 1][i + 1])) {
+                return new int[]{matrix.length - 1, i};
+            }
+        }
+        if ((matrix[matrix.length - 1][matrix[0].length - 1] > matrix[matrix.length - 1][matrix[0].length - 2]) &&
+                (matrix[matrix.length - 1][matrix[0].length - 1] > matrix[matrix.length - 2][matrix[0].length - 1])) {
+            return new int[]{matrix.length - 1, matrix[0].length - 1};
+        }
+        return new int[]{-1, -1};
+    }
+
+//    if user wants get transposed matrix and save initial matrix
+//    then this function should not change the argument and we are
+//    forced to use additional memory
     public static double[][] transpose(double[][] matrix) {
-        for (int row = 0; row < matrix.length - 1; row++) {
-            if (matrix[row].length != matrix[row + 1].length) {
-                throw new IllegalArgumentException("Array must be matrix");
+        if (matrix == null) {
+            throw new NullPointerException("Matrix can`t be null");
+        }
+        for (double[] row : matrix) {
+            if (row == null) {
+                throw new NullPointerException("Row of matrix can`t be null");
             }
         }
+        if (!isMatrix(matrix)) {
+            throw new IllegalArgumentException("Array must be matrix");
+        }
+
         double[][] transposedMatrix = new double[matrix[0].length][matrix.length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
@@ -139,6 +327,26 @@ public class Matrix {
             }
         }
         return transposedMatrix;
+    }
+
+    private static boolean isMatrix(double[][] array) {
+        for (int row = 0; row < array.length - 1; row++) {
+            if (array[row].length != array[row + 1].length) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isOneValue(double[][] matrix) {
+        for (double[] row : matrix) {
+            for (double cell : row) {
+                if (cell != matrix[0][0]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
