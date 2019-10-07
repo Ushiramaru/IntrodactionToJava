3334167703d847a3fea72f0f1e85415e5b94aee5
Асус <https://github.com/Ushiramaru/OOP_1.git>
2019-10-07 13:11:34 +0300

    Added JSON deserialization

--- src/mainTask_4/serialization/JSONSerializer.java
+++ src/mainTask_4/serialization/JSONSerializer.java
@@ -1,12 +1,32 @@
 package mainTask_4.serialization;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 import mainTask_4.Present;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class JSONSerializer implements Serializer {
+
+    public class JsonDeserializerWithInheritance<Shape> implements JsonDeserializer<Shape> {
+
+        @Override
+        public Shape deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            JsonObject jsonObject = json.getAsJsonObject();
+            JsonPrimitive classNamePrimitive = (JsonPrimitive) jsonObject.get("type");
+            String className = classNamePrimitive.getAsString();
+
+            Class<?> clazz;
+            try {
+                clazz = Class.forName(className);
+            } catch (ClassNotFoundException e) {
+                return null;
+            }
+            return context.deserialize(jsonObject, clazz);
+        }
+
+    }
 
     @Override
     public String serialize(ArrayList<Present> presents) {
@@ -16,8 +36,21 @@ public class JSONSerializer implements Serializer {
     }
 
     @Override
-    public ArrayList<Present> deserialize(String path) {
-        return null;
+    public ArrayList<Present> deserialize(String json) {
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(Present.class, new JsonDeserializerWithInheritance<Present>());
+        Gson gson = builder.setPrettyPrinting().create();
+
+        Type itemsArrType = new TypeToken<Present[]>() {}.getType();
+        Present[] arrItemsDes = gson.fromJson(json, itemsArrType);
+
+        ArrayList<Present> presents = new ArrayList<>();
+        for (Present arrItemsDe : arrItemsDes) {
+            if (arrItemsDe != null) {
+                presents.add(arrItemsDe);
+            }
+        }
+        return presents;
     }
 
 }
